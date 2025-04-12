9840e646eb19a7575eb192ed166c36aca6e71212
Ananta Chandra Das <danantach@gmail.com>
2025-04-12 19:27:16 +0530

    .

--- public/js/map.js
+++ public/js/map.js
@@ -1,12 +1,24 @@
 let mapToken = map_Token;
 mapboxgl.accessToken = mapToken;
-  const map = new mapboxgl.Map({
-      container: 'map', // container ID
-      center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
-      zoom: 9 // starting zoom
-  });
-
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    center: coordinates, // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
 
 const marker = new mapboxgl.Marker({color: "red"})
   .setLngLat(coordinates)
   .addTo(map);
+
+// Add map controls
+map.addControl(new mapboxgl.NavigationControl());
+
+// Make sure the map resizes properly when window is resized
+window.addEventListener('resize', () => {
+  map.resize();
+});
+
+// Ensure the map container fits its bounds when loaded
+map.on('load', function() {
+  map.resize();
+});
